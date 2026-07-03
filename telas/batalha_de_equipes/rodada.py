4f136ec2df76797c909462f6647e198e3aab055d
Jackson Vieira <jacksonvieira.santos@gmail.com>
2026-07-03 09:06:02 -0300

    Fix: corrigida funcionalidade do módulo de batalha de equipes

--- telas/batalha_de_equipes/rodada.py
+++ telas/batalha_de_equipes/rodada.py
@@ -9,7 +9,7 @@ from services.batalha_service import (
 )
 from utils.estilo import aplicar_estilo
 
-@st.fragment(run_every=3)
+@st.fragment(run_every=2)
 def painel_estatistico_reativo(batalha_id, time_a_id, time_b_id, nome_ta, nome_tb):
     pa, pb = calcular_placar_atual(batalha_id, time_a_id, time_b_id)
     st.markdown(f"**Placar:** {nome_ta} {pa} vs {nome_tb} {pb}", unsafe_allow_html=True)
